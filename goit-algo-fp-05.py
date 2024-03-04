72f2cd5cff514b2979052331c95b6d7c1ffa6af1
N1nochka <dewi@ukr.net>
2024-03-05 00:01:14 +0100

    task5

--- goit-algo-fp-05.py
+++ goit-algo-fp-05.py
@@ -1,81 +1,87 @@
+import uuid
 import networkx as nx
 import matplotlib.pyplot as plt
-import uuid
-import matplotlib.colors as mcolors
 
 class Node:
     def __init__(self, key):
         self.left = None
         self.right = None
         self.val = key
+        self.id = str(uuid.uuid4())
 
-def add_edges(graph, node, pos, x=0, y=0, layer=1, max_layer=1):
-    if node is not None:
-        color = mcolors.to_hex(plt.cm.viridis(layer / max_layer))  # Генерація колірного коду
-        if node.val == 1:  
-            color = "#00008B"
-        graph.add_node(node.val, color=color, label=node.val)
-        if node.left:
-            graph.add_edge(node.val, node.left.val)
-            l = x - 1 / 2 ** layer
-            pos[node.left.val] = (l, y - 1)
-            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1, max_layer=max_layer)
-        if node.right:
-            graph.add_edge(node.val, node.right.val)
-            r = x + 1 / 2 ** layer
-            pos[node.right.val] = (r, y - 1)
-            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1, max_layer=max_layer)
+    def add_edges(self, graph, node, pos, x=0, y=0, layer=1):
+        if node is not None:
+            graph.add_node(node.id, label=node.val)
+            if node.left:
+                graph.add_edge(node.id, node.left.id)
+                l = x - 1 / 2 ** layer
+                pos[node.left.id] = (l, y - 1)
+                l = self.add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+            if node.right:
+                graph.add_edge(node.id, node.right.id)
+                r = x + 1 / 2 ** layer
+                pos[node.right.id] = (r, y - 1)
+                r = self.add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+        return graph
 
-def draw_tree(tree_root):
-    tree = nx.DiGraph()
-    pos = {tree_root.val: (0, 0)}
-    add_edges(tree, tree_root, pos, max_layer=max_depth(tree_root))
-    
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+    def draw_tree(self, traversal_type, font_size=10):
+        tree = nx.DiGraph()
+        pos = {self.id: (0, 0)}
+        tree = self.add_edges(tree, self, pos)
 
-    plt.figure(figsize=(10, 6))
-    nx.draw(tree, pos=pos, labels=labels, node_color=colors, node_size=2000, with_labels=True, font_size=10)
-    plt.show()
+        # Генеруємо послідовність кольорів для вузлів
+        colors = self.generate_colors(traversal_type)
 
-def dfs(node, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(node.val)
-    print(node.val)
-    if node.left and node.left.val not in visited:
-        dfs(node.left, visited)
-    if node.right and node.right.val not in visited:
-        dfs(node.right, visited)
+        labels = {node[0]: node[1]["label"] for node in tree.nodes(data=True)}
+        plt.figure(figsize=(8, 5))
+        nx.draw(
+            tree,
+            pos=pos,
+            labels=labels,
+            arrows=False,
+            node_size=2500,
+            node_color=colors,  
+            font_size=font_size,
+            font_color="white"  
+        )
+        plt.show()
 
-def bfs(node):
-    visited = set()
-    queue = [node]
-    while queue:
-        current_node = queue.pop(0)
-        visited.add(current_node.val)
-        print(current_node.val)
-        if current_node.left and current_node.left.val not in visited:
-            queue.append(current_node.left)
-        if current_node.right and current_node.right.val not in visited:
-            queue.append(current_node.right)
+    def generate_colors(self, traversal_type):
+        if traversal_type == "DFS":
+            base_color = (0.05, 0.588, 0.941)  
+        elif traversal_type == "BFS":
+            base_color = (0.8, 0, 0)  
 
-def max_depth(node):
-    if node is None:
-        return 0
-    else:
-        left_depth = max_depth(node.left)
-        right_depth = max_depth(node.right)
-        return max(left_depth, right_depth) + 1
+        colors = []
+        num_colors = 15
+        for i in range(num_colors):
+            intensity = 0.07 * i  
+            color = (
+                max(0, base_color[0] - intensity),
+                max(0, base_color[1] - intensity),
+                max(0, base_color[2] - intensity),
+            )
+            colors.append(color)
+        return colors[::-1]  
 
-# Приклад використання
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
+def main():
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.left.left.left = Node(8)
+    root.left.left.right = Node(9)
+    root.left.right.left = Node(10)
+    root.left.right.right = Node(11)
+    root.right = Node(3)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.right.left.left = Node(12)
+    root.right.left.right = Node(13)
+    root.right.right.left = Node(14)
+    root.right.right.right = Node(15)
+    root.draw_tree(traversal_type="DFS")  
+    root.draw_tree(traversal_type="BFS")  
 
-# Відображення дерева
-draw_tree(root)
+if __name__ == "__main__":
+    main()
